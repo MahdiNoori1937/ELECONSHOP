1d47be575bd5aa6717a56b6d7082fe71392f8338
Mahdi Noori <mr.mahdinoori1378@gmail.com>
2025-07-28 16:33:54 +0430

    add Indes

---
+++ ELECON.Presentation/Controllers/Home.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELECON.Presentation.Controllers;
+
+public class Home : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
