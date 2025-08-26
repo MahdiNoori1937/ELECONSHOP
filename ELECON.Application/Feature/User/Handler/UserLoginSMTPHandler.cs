e2e3e10f216da2ce375c92f39f6a97397462fb54
Mahdi Noori <mr.mahdinoori1378@gmail.com>
2025-08-26 09:46:34 +0430

    some changhes1

---
+++ ELECON.Application/Feature/User/Handler/UserLoginSMTPHandler.cs
@@ -0,0 +1,57 @@
+﻿using ELECON.Application.Feature.User.Command;
+using ELECON.Application.Feature.User.DTOs;
+using ELECON.Domain.Entities.User;
+using ELECON.Domain.Interface.IUserRepository;
+using MediatR;
+
+namespace ELECON.Application.Feature.User.Handler;
+
+public class UserLoginSMTPHandler : IRequestHandler<LoginUserWithSMTPDtoCommand, UserSMTPLoginStatus>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUserSecurityRepository _userSecurityRepository;
+
+    public UserLoginSMTPHandler(IUserRepository userRepository, IUserSecurityRepository userSecurityRepository)
+    {
+        _userRepository = userRepository;
+        _userSecurityRepository = userSecurityRepository;
+    }
+
+    public async Task<UserSMTPLoginStatus> Handle(LoginUserWithSMTPDtoCommand request,
+        CancellationToken cancellationToken)
+    {
+        UserSecurity userSecurity =
+            await _userSecurityRepository.GetUserSecurityByInput(request.SmtpCodeDto.RegisterInput);
+        if (userSecurity == null)
+        {
+            return UserSMTPLoginStatus.UserNotFound;
+        }
+
+        if (userSecurity.LockoutEnd < DateTime.Now)
+        {
+            return UserSMTPLoginStatus.UserLockout;
+        }
+
+        if (userSecurity.SMTPCode != request.SmtpCodeDto.SMTPCode)
+        {
+            userSecurity.LastFailedLogin = DateTime.Now;
+            userSecurity.FailedLoginAttempts++;
+            if (userSecurity.FailedLoginAttempts > 3)
+            {
+                userSecurity.LockoutEnd = DateTime.Now.AddMinutes(3);
+                userSecurity.IsLockedOut = true;
+                await _userSecurityRepository.Update(userSecurity);
+                return UserSMTPLoginStatus.UserGotTimeOut;
+            }
+
+            await _userSecurityRepository.Update(userSecurity);
+            return UserSMTPLoginStatus.SMTPCodeFailed;
+        }
+
+        userSecurity.FailedLoginAttempts = 0;
+        userSecurity.LockoutEnd = null;
+        userSecurity.IsLockedOut = false;
+        await _userSecurityRepository.Update(userSecurity);
+        return UserSMTPLoginStatus.Success;
+    }
+}
---
+++ ELECON.Application/Feature/User/Handler/UserRegisterHandler.cs
@@ -0,0 +1,63 @@
+﻿using ELECON.Application.Extensions;
+using ELECON.Application.Feature.User.Command;
+using ELECON.Application.Feature.User.DTOs;
+using ELECON.Application.Security;
+using ELECON.Domain.Entities.User;
+using ELECON.Domain.Interface.IUserRepository;
+using MediatR;
+
+namespace ELECON.Application.Feature.User.Handler;
+
+public class UserRegisterHandler(
+    IUserRepository _userRepository,
+    IUserSecurityRepository _userSecurityRepository,
+    UserSendEmail _emailSender,
+    UserSendSms _userSendSms) : IRequestHandler<UserRegisterCommand, CheckUserRegisterStatus>
+{
+    public async Task<CheckUserRegisterStatus> Handle(UserRegisterCommand request, CancellationToken cancellationToken)
+    {
+        string Input = request.UserRegisterUserDto.RegisterInput;
+        string code = 5.ChooseRandomNumber().ToString();
+        Domain.Entities.User.User user =
+            await _userRepository.FindByEmailOrNumberAsync(request.UserRegisterUserDto.RegisterInput);
+        if (user != null)
+            return CheckUserRegisterStatus.InputExists;
+
+        if (request.UserRegisterUserDto.Password != null)
+            user = new Domain.Entities.User.User
+            {
+                UserStatus = "NotActive",
+                RoleId = 1,
+                Pasword = SecretHasher.Hash(request.UserRegisterUserDto.Password)
+            };
+        if (Input.EmailIsValid())
+        {
+            user.Email = Input;
+            await _emailSender.Send(user, code);
+        }
+        else
+        {
+            user.PhoneNumber = Input;
+            await _userSendSms.Send(user, code);
+        }
+
+        (string addUserResult, int userId) = await _userRepository.Add(user);
+        if (!EnumExtensions.IsStatusReached<UserRegisterStatus>(addUserResult))
+        {
+            return CheckUserRegisterStatus.Failed;
+        }
+
+        (string addSecurityResult, int secId) = await _userSecurityRepository.Add(new UserSecurity
+        {
+            FailedLoginAttempts = 0,
+            SMTPCode = code,
+            UserId = userId,
+        });
+        if (!EnumExtensions.IsStatusReached<AddUserSecurityStatus>(addSecurityResult))
+        {
+            return CheckUserRegisterStatus.Failed;
+        }
+
+        return CheckUserRegisterStatus.Success;
+    }
+}
